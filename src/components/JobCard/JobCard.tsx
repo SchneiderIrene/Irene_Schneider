bd72e00221cd4a1ac71915bf4d421a30a8f2622d
Schneider Irene <schneider_irene@outlook.com>
2024-08-16 10:25:44 +0200

    tooltip

--- src/components/JobCard/JobCard.tsx
+++ src/components/JobCard/JobCard.tsx
@@ -1,5 +1,4 @@
-
-import { useState } from "react";
+import { useState } from "react"
 //import { VscDebugStepBack } from "react-icons/vsc";
 import {
   ButtonControl,
@@ -20,7 +19,8 @@ import {
 } from "./styles"
 import { JobDescription, JobProps } from "./types"
 import { Skip } from "assets"
-import { useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next"
+import Tooltip from "@mui/material/Tooltip"
 
 function JobCard({ jobs }: JobProps) {
   const [index, setIndex] = useState(0)
@@ -41,40 +41,44 @@ function JobCard({ jobs }: JobProps) {
     setIndex(0)
   }
 
-  const {t} = useTranslation();
+  const { t } = useTranslation()
 
-  const descriptions =  t(`jobs.${index}.description`, { returnObjects: true }) as JobDescription[];
+  const descriptions = t(`jobs.${index}.description`, {
+    returnObjects: true,
+  }) as JobDescription[]
 
-  descriptions.map((desc) => ({
-    titleDescription: desc.titleDescription ? t(desc.titleDescription) : '',
+  descriptions.map(desc => ({
+    titleDescription: desc.titleDescription ? t(desc.titleDescription) : "",
     spans: desc.span ? desc.span.map(span => t(span)) : [],
-    listItems: desc.li ? desc.li.map(liItem => t(liItem)) : []
-  }));
-  
+    listItems: desc.li ? desc.li.map(liItem => t(liItem)) : [],
+  }))
 
   return (
     <JobCardWrapper>
       <ButtonControl position>
-        <IconBox
-          src={Skip}
-          onClick={handlePrev}
-          disabled={index === 0}
-          scaleX
-        />
-        <ButtonStart 
-        title="start" 
-        onClick={handleReset}>
-         <GoDotFillComponent />
-          <GoDotFillComponent />
-          <GoDotFillComponent />
-          {/* <RiRestartLine color="rgba(252, 241, 228, 0.9)" fontSize={"1.5vw"}/> */}
-        </ButtonStart>
-
-        <IconBox
-          src={Skip}
-          onClick={handleNext}
-          disabled={index === jobs.length - 1}
-        />
+        <Tooltip title={t("tooltips.tooltipBack")}>
+          <IconBox
+            src={Skip}
+            onClick={handlePrev}
+            disabled={index === 0}
+            scaleX
+          />
+        </Tooltip>
+        <Tooltip title={t("tooltips.tooltipStart")}>
+          <ButtonStart onClick={handleReset}>
+            <GoDotFillComponent />
+            <GoDotFillComponent />
+            <GoDotFillComponent />
+            {/* <RiRestartLine color="rgba(252, 241, 228, 0.9)" fontSize={"1.5vw"}/> */}
+          </ButtonStart>
+        </Tooltip>
+        <Tooltip title={t("tooltips.tooltipNext")}>
+          <IconBox
+            src={Skip}
+            onClick={handleNext}
+            disabled={index === jobs.length - 1}
+          />
+        </Tooltip>
       </ButtonControl>
       <CardComponent>
         <TitleH2Card>
@@ -83,7 +87,8 @@ function JobCard({ jobs }: JobProps) {
             {" "}
             {t(`jobs.${index}.company`)}
           </StyledA>{" "}
-          &nbsp; {t(`jobs.${index}.companyDescription`)}, {t(`jobs.${index}.location`)}
+          &nbsp; {t(`jobs.${index}.companyDescription`)},{" "}
+          {t(`jobs.${index}.location`)}
         </TitleH2Card>
         <TitleH3Card>{t(`jobs.${index}.period`)}</TitleH3Card>
         <TitleH4Card>{t(`jobs.${index}.responsibilities`)}</TitleH4Card>
@@ -97,9 +102,9 @@ function JobCard({ jobs }: JobProps) {
                 desc.li.map((liItem, k) => (
                   <StyledLi key={k}>
                     {desc.span && desc.span[k] && (
-                    <StyledSpan>{desc.span[k]} &nbsp;</StyledSpan>
-                  )}
-                  {liItem}
+                      <StyledSpan>{desc.span[k]} &nbsp;</StyledSpan>
+                    )}
+                    {liItem}
                   </StyledLi>
                 ))}
             </Part>
@@ -113,9 +118,7 @@ function JobCard({ jobs }: JobProps) {
           disabled={index === 0}
           scaleX
         />
-        <ButtonStart 
-        title="zum Anfang" 
-        onClick={handleReset}>
+        <ButtonStart title="zum Anfang" onClick={handleReset}>
           <GoDotFillComponent />
           <GoDotFillComponent />
           <GoDotFillComponent />
